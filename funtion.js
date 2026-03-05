85b4a2893b4b3d1a219139d5a64822b3e8c40022
Riddhima-gangwar <riddhima.gangwar9@gmail.com>
2026-03-05 22:26:32 +0530

    first commit

--- funtion.js
+++ funtion.js
@@ -1,11 +1,11 @@
-function addition(a,b)
-{
-    return a+b
+function addition(a, b) {
+    return a + b;
+}
 
+function sub(a, b) {
+    return a - b;
 }
-function sub(a,b)
-{
-    return a-b
-}
-console.log(addition(7,2))
-console.log(sub(8,1))
+
+module.exports = { addition, sub };
+// console.log(addition(7,2))
+// console.log(sub(8,1))
---
+++ math.js
@@ -0,0 +1,4 @@
+const math = require('./funtion.js');
+
+console.log(math.addition(7, 2));
+console.log(math.sub(8, 1));
